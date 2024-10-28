ed8541d94de708914796d9d31e05c96f419181d7
UltGab04 <gabriel.workDev.UNI2004@gmail.com>
2024-10-28 17:28:45 -0300

    Terminar

---
+++ scripts/menuPrincipal.js
@@ -0,0 +1,11 @@
+function laudosMedicos() {
+    alert("Abrindo seus laudos prontos...");
+}
+
+function mostrarConsultas(){
+    alert("Verificando consultas marcadas...")
+}
+
+function remarcarConsultas(){
+    alert("Abrindo remarcar consultas...")
+}
