c73256ffc39b36503c667be20b56574b3df98673
gustavobarez <gbarez44@gmail.com>
2024-11-02 11:35:16 -0300

    mouse again

--- scripts/redefinirSenha.js
+++ scripts/redefinirSenha.js
@@ -1,3 +1,15 @@
 function enviarEmail(){
     alert("Mensagem de redefinição senha foi enviada para sua caixa principal no email! ");
 }
+
+const fonteCabecario = document.querySelectorAll('.cab');
+
+fonteCabecario.forEach(function(elemento) {
+  elemento.addEventListener('mouseover', function() {
+    elemento.style.color = 'cyan'; 
+});
+
+elemento.addEventListener('mouseout', function() {
+  elemento.style.color = 'black';
+});
+});
